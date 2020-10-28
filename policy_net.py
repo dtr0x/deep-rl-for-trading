Update to ex ante sigma computation with correct dimensions. Use batch_first data inputs for policy net.

policy_net.py
@@ -16,20 +16,17 @@ class PolicyNet(nn.Module):
             input_size=input_dim,
             hidden_size=hidden_dim,
             num_layers=2,
-            dropout=drop_rate
+            dropout=drop_rate,
+            batch_first=True
             )
 
         # discrete action space
         self.linear = nn.Linear(hidden_dim, 3)
         self.softmax = nn.Softmax(dim=1)
 
-    # define forward pass through LSTM with single output in (-1, 1)
     # assumed input size: batch_size * seq_len * input_dim
     def forward(self, input):
-        # reshape input for LSTM input dimensions
-        i = input.view(seq_len, -1, input_dim)
-
-        output, h = self.lstm(i)
+        output, h = self.lstm(input)
 
         ff_input = h[0][1] # get second layer hidden output
 

reinforcement.py
@@ -16,10 +16,10 @@ def ex_ante_sigma(prices, t0):
 
 # pre-compute ex ante sigmas for all days/assets
 def get_sigma_all(prices, t0):
-    n_assets = prices.shape[1]
+    n_assets = len(prices)
     f = lambda x: ex_ante_sigma(x, t0) # function to compute sigma over price series
-    sigall = np.apply_along_axis(f, 0, prices)
-    return torch.tensor(sigall, dtype=torch.float32).view(n_assets, -1, 2)
+    sigall = np.apply_along_axis(f, 1, prices)
+    return torch.tensor(sigall, dtype=torch.float32)
 
 # Compute reward (Equation (4) in 'Deep Reinforcement Learning for Trading' with mu=1).
 def reward(prices, prices_next, sigma, actions, actions_prev, tgt_vol, bp):
@@ -33,6 +33,6 @@ if __name__ == '__main__':
     data = pd.read_csv('cleaned_data.csv')
     # initial time index: compute and save states starting from 2005
     t0 = data[data['date'] >= '2005'].index[0]
-    prices = np.array(data.drop(columns='date'), dtype='float32')
+    prices = np.array(data.drop(columns='date'), dtype='float32').transpose()
     sigall = get_sigma_all(prices, t0)
     torch.save(sigall, 'ex_ante_sigma.pt')

validation.py
@@ -2,6 +2,7 @@ import torch
 from policy_net import PolicyNet
 from save_states_all import load_states
 from reinforcement import reward
+import
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,8 +22,8 @@ if __name__ == '__main__':
 
     returns = []
     for t in val_idx:
-        states = S[:, t].contiguous()
-        next_states = S[:, t+1].contiguous()
+        states = S[:, t]
+        next_states = S[:, t+1]
         prices = P[:, t]
         prices_next = P[:, t+1]
         sigmas = sigall[:, t]
@@ -30,3 +31,5 @@ if __name__ == '__main__':
         actions = model.get_actions(states)
         rewards = reward(prices, prices_next, sigmas, actions, actions_prev, tgt_vol, bp)
         returns.append(rewards.mean().item())
+
+    crets = np.cumsum(returns)