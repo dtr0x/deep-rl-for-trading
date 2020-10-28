bac3507e0829a23d6c080ff919f41c746ccb612b
Dylan Troop <dylantroop@gmail.com>
2020-10-28 16:22:37 +0100

    Adjustment to policy network.

--- policy_net.py
+++ policy_net.py
@@ -3,40 +3,25 @@ import torch.nn as nn
 
 # Parameters
 input_dim = 7
-hidden_dim1 = 64
-hidden_dim2 = 32
-drop_rate = 0.2
+hidden_dim = 52
+drop_rate = 0.5
 seq_len = 60
 
 class PolicyNet(nn.Module):
-    def __init__(self, is_dqn=True):
+    def __init__(self):
         super(PolicyNet, self).__init__()
-        self.is_dqn = is_dqn
 
         # Define the first LSTM layer
-        self.lstm1 = nn.LSTM(
+        self.lstm = nn.LSTM(
             input_size=input_dim,
-            hidden_size=hidden_dim1)
+            hidden_size=hidden_dim,
+            num_layers=2,
+            dropout=drop_rate
+            )
 
-        # dropout some outputs to avoid overfitting
-        self.dropout = nn.Dropout(drop_rate)
-
-        # apply leaky ReLU activation to outputs of dropout
-        self.leaky_relu = nn.LeakyReLU()
-
-        # Define the first LSTM layer
-        self.lstm2 = nn.LSTM(
-            input_size=hidden_dim1,
-            hidden_size=hidden_dim2)
-
-        if self.is_dqn:
-            # discrete action space
-            self.linear = nn.Linear(hidden_dim2, 3)
-            self.softmax = nn.Softmax(dim=1)
-        else:
-            # continuous action space
-            self.linear = nn.Linear(hidden_dim2, 1)
-            self.tanh = nn.Tanh()
+        # discrete action space
+        self.linear = nn.Linear(hidden_dim, 3)
+        self.softmax = nn.Softmax(dim=1)
 
     # define forward pass through LSTM with single output in (-1, 1)
     # assumed input size: batch_size * seq_len * input_dim
@@ -44,30 +29,13 @@ class PolicyNet(nn.Module):
         # reshape input for LSTM input dimensions
         i = input.view(seq_len, -1, input_dim)
 
-        output, _ = self.lstm1(i)
-        # print("lstm layer 1 output size: ", output.size())
+        output, h = self.lstm(i)
 
-        output = self.dropout(output)
-        # print("dropout layer output size: ", output.size())
-
-        output = self.leaky_relu(output)
-        # print("relu layer output size: ", output.size())
-
-        # the output of the last LSTM in the chain (h) is passed to linear
-        output, h = self.lstm2(output)
-        # print("lstm layer 2 output size: ", output.size())
-
-        ff_input = h[0].squeeze()
-        # print("ff input size: ", ff_input.size())
+        ff_input = h[0][1] # get second layer hidden output
 
         output = self.linear(ff_input)
-        # ("linear layer output size: ", output.size())
 
-        if self.is_dqn:
-            output = self.softmax(output)
-        else:
-            # scale output to (-1, 1)
-            output = self.tanh(output)
+        output = self.softmax(output)
 
         return output
 
@@ -85,3 +53,4 @@ if __name__ == '__main__':
     out = dqn(t)
 
     print(out)
+    print(dqn.get_actions(t))
