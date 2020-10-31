39eadff5724b28e3f4a399a3e9b74eadab4a5f43
Dylan Troop <dylantroop@gmail.com>
2020-10-31 21:10:20 +0100

    Changes to load data script, counting consecutive NaN values.

--- load_data.py
+++ load_data.py
@@ -1,39 +1,52 @@
 import pickle
-from util import *
+import pandas as pd
 
-# load time series data and save as tensors
+# return the maximum length period of empty data from price series
+def max_nan_len(prices):
+    pna = prices.isna()
+    max_na_count = 0
+    na_count = 0
+    for i in pna:
+        if i:
+            na_count += 1
+        else:
+            if na_count > max_na_count:
+                max_na_count = na_count
+                na_count = 0
+    return max_na_count
+
+# forward fill nan values in price series (in-place)
+def forward_fill(prices):
+    idx = prices.index
+    i = idx[0]
+    for j in idx[1:]:
+        if pd.isna(prices[j]):
+            prices[j] = prices[i]
+        i = j
+    return prices
 
 if __name__ == '__main__':
     # load dataframe stored in pkl file
     with open('raw.pkl', 'rb') as f:
         data = pickle.load(f)
 
-    # make 'date' index a column
-    data = data.reset_index()
-
-    # remove 'ticker' column name
-    data.columns.name = None
-
-    # drop columns with too many NaNs
-    data = data.drop(columns=['WN1 Comdty', 'QC1 Index', 'XB1 Comdty', 'ST1 Index'])
+    # remove columns with too many NaNs starting from 2004
+    data = data[data.index >= '2004']
+    nan_lens = data.agg(max_nan_len)
+    cols = nan_lens[nan_lens <= 20].index
+    data = data[cols]
 
     # get first date where no columns have NaNs
-    init_date = data.dropna()['date'].tolist()[0]
+    init_date = data.dropna().index[0]
     # reset first date
-    data = data[data['date'] >= init_date]
-    data = data.reset_index(drop=True)
-    
-    # forward filling all NA values
-    n = data.shape[0]
-    k = data.shape[1]-1
-    colnames = data.columns[1:]
-    for tt in range(0,k):
-        data_vec = data[colnames[tt]]
-        data_vec = data_vec.reset_index(drop=True)
-        data_vec_na = data_vec.isna()
-        for i in range(1,n):
-            if data_vec_na[i] == True:
-                data[colnames[tt]][i] = data[colnames[tt]][i-1]
+    data = data[data.index >= init_date]
 
+    # forward fill all asset columns
+    data.agg(forward_fill)
+
+    # make 'date' index a column
+    data = data.reset_index()
+    # remove 'ticker' column name
+    data.columns.name = None
     # saving data to csv
     data.to_csv('cleaned_data.csv', index=False)
