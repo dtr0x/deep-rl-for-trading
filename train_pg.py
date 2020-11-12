d003968f2e619afb475db597b39cb99cf7e37c49
Dylan Troop <dylantroop@gmail.com>
2020-11-12 19:53:50 +0000

    Policy gradient training and validation graphs.

--- train_pg.py
+++ train_pg.py
@@ -2,46 +2,38 @@ import random, torch
 import numpy as np
 from save_states_all import load_states
 from policy_net import PolicyNet
-from reinforcement import reward
-import time
+from reinforcement import get_reward
+from torch.nn.functional import softmax
+import time, argparse
 
 # set device for optimization
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Parameters
-discount_factor = 0.3
-learning_rate = 1e-4
-num_epochs = 20
-# for computing rewards (on-device)
-bp = torch.tensor(2e-3, device=device)
-tgt_vol = torch.tensor(0.1, device=device)
+def train_pg(policy_net, optimizer, asset_type, min_year=2006, max_year=2010,
+    val_frac=0.1, discount_factor=0.3, n_epochs=20, bp=0.002, tgt_vol=0.1, num_epochs=20):
+    # durations to train/validate for
+    min_year_idx = min_year % 2006 + 1
+    max_year_idx = max_year % 2006 + 1
+    min_idx = 252*(min_year_idx-1)
+    max_idx = 252*max_year_idx - 1
+    val_idx_min = int((1-val_frac) * (max_idx-min_idx)) + min_idx
+    train_idx = range(min_idx, val_idx_min)
+    val_idx = range(val_idx_min, max_idx)
 
-policy_net = PolicyNet().to(device)
-
-optimizer = torch.optim.Adam(policy_net.parameters(), lr=learning_rate)
-
-if __name__ == '__main__':
-    # duration to train for, approximately 5 years 2010-2014
-    train_idx = range(1000,2500)
-    # load commodoties data
-    S, P, sigall = [x.to(device) for x in load_states('commodity')]
-    nidx = [7,16,20,25,27,32,35]
-    idx = [i for i in np.arange(len(S)) if i not in nidx]
-    S = S[idx]
-    P = P[idx]
-    sigall = sigall[idx]
-
-    n_assets = len(S)
     n_steps = len(train_idx)
+
+    # load data
+    S, P, sigall = [x.to(device) for x in load_states(asset_type)]
+    n_assets = len(S)
 
     actions_prev = torch.zeros(n_assets, device=device)
 
     for i_epoch in range(num_epochs):
+        policy_net.train()
         t1 = time.time()
         # Store rewards and actions for this trajectory (epoch)
         rewards_all = torch.zeros((n_steps, n_assets), dtype=torch.float32, device=device)
         actions_all = torch.zeros((n_steps, n_assets), dtype=torch.long, device=device)
-
 
         step = 0 # for indexing rewards/actions
         # loop to store trajectories
@@ -51,14 +43,14 @@ if __name__ == '__main__':
             next_states = S[:, t+1]
             prices = P[:, t]
             prices_next = P[:, t+1]
-            sigmas = sigall[:, t]
-
+            sigmas = sigall[:, t+1]
+            sigmas_prev = sigall[:, t]
 
             # get actions for each asset
             actions = policy_net.get_actions(states)
 
             # compute rewards for each asset
-            rewards = reward(prices, prices_next, sigmas, actions, actions_prev, tgt_vol, bp)
+            rewards = get_reward(prices, prices_next, sigmas, sigmas_prev, actions, actions_prev, tgt_vol, bp)
 
             actions_all[step] = actions
             rewards_all[step] = rewards
@@ -81,23 +73,76 @@ if __name__ == '__main__':
 
             # get values of currrent actions from policy net
             action_values = policy_net(states)
-            action_idx = actions + 1 # action indexes
-            row_idx = torch.arange(action_values.size(0))
-            action_values = action_values[row_idx, action_idx]
-            log_action_values = action_values.log()
+            probs = softmax(action_values, dim=1)
+            action_idx = actions.long() + 1 # action indexes
+            row_idx = torch.arange(probs.size(0))
+            probs = probs[row_idx, action_idx]
+            log_probs = probs.log()
 
-            policy_grad_op = discount_factor**step * g * log_action_values
+            policy_grad_op = discount_factor**step * g * log_probs
 
             optimizer.zero_grad() # set gradients to 0 to avoid accumulation
 
-            policy_grad_op.backward(torch.ones_like(policy_grad_op)) # backpropograte to compute gradient wrt parameters
+            policy_grad_op.sum().backward() # backpropograte to compute gradient wrt parameters
 
             optimizer.step() # perform gradient descent step
 
             actions_prev = actions
             step += 1
+
+        # validation loop
+        val_returns = []
+        actions_prev = torch.zeros(len(S), device=device)
+        policy_net.eval() # eval mode for validation
+        for t in val_idx:
+            # get states/prices/sigma values for each asset
+            states = S[:, t]
+            next_states = S[:, t+1]
+            prices = P[:, t]
+            prices_next = P[:, t+1]
+            sigmas = sigall[:, t+1]
+            sigmas_prev = sigall[:, t]
+            actions = policy_net.get_actions(states) # get actions from DQN
+            # compute rewards
+            rewards = get_reward(prices, prices_next, sigmas, sigmas_prev,
+            actions, actions_prev, tgt_vol, bp)
+            val_returns.append(rewards.mean().item())
+            # store actions for next time step to compute reward
+            actions_prev = actions
+        print("Last actions chosen on validation set:", actions)
+
+        # compare the average reward received on val set for early stopping
+        avg_val_return = np.mean(val_returns)
+        print("Average return on validation set at epoch {}: {:.3f}".format(i_epoch, avg_val_return))
+        if i_epoch == 0:
+            best_return = avg_val_return
+            torch.save(policy_net.state_dict(), "models/pg_{}_{}_{}.pt".format(asset_type, min_year, max_year))
+        elif avg_val_return > best_return:
+            best_return = avg_val_return
+            torch.save(policy_net.state_dict(), "models/pg_{}_{}_{}.pt".format(asset_type, min_year, max_year))
+
         t2 = time.time()
         t = (t2-t1)/60
-        print("Finished epoch {} in {:.2f} minutes".format(i_epoch+1, t))
-        # save the model every epoch
-        torch.save(policy_net.state_dict(), "models/pg_epoch_{}.pt".format(i_epoch+1))
+        av_ret_epoch = rewards_all.mean()
+        print("Finished epoch {} in {:.2f} minutes. Average return: {:.3f}".format(i_epoch, t, av_ret_epoch))
+        i_epoch += 1
+
+if __name__ == '__main__':
+    # get arguments from command line for asset type and year range
+    parser = argparse.ArgumentParser(description='Select asset type and year range to train.')
+    parser.add_argument('-asset_type', type=str, required=True)
+    parser.add_argument('-min_year', type=int, required=True)
+    parser.add_argument('-max_year', type=int, required=True)
+    args = parser.parse_args()
+    asset_type = args.asset_type
+    min_year = args.min_year
+    max_year = args.max_year
+
+    # set up networks, memory, optimizer
+    policy_net = PolicyNet().to(device)
+    optimizer = torch.optim.Adam(policy_net.parameters(), lr=1e-4)
+
+    print("Training Policy Gradient on {} assets for years {} to {}...".format(asset_type, min_year, max_year))
+
+    # train and save best params
+    train_pg(policy_net, optimizer, asset_type, min_year, max_year)
--- validation.py
+++ validation.py
@@ -9,11 +9,11 @@ import argparse
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def signR(prices, t):
-    rets = (prices[:, t-251:t+1] - prices[:, t-252:t])/prices[:, t-252:t]
+    rets = (prices[:, t-251:t+1] - prices[:, t-252:t])
     return rets.mean(axis=1).sign()
 
 if __name__ == '__main__':
-    get arguments from command line for asset type and year range
+    # get arguments from command line for asset type and year range
     parser = argparse.ArgumentParser(description='Select asset type and year range to train.')
     parser.add_argument('-asset_type', type=str, required=True)
     parser.add_argument('-min_year', type=int, required=True)
@@ -23,10 +23,15 @@ if __name__ == '__main__':
     min_year = args.min_year
     max_year = args.max_year
 
-    # load params and set model to eval mode
-    params1 = torch.load("models/dqn_{}_{}_{}.pt".format(asset_type, 2006, 2010))
-    params2 = torch.load("models/dqn_{}_{}_{}.pt".format(asset_type, 2011, 2015))
-    model = PolicyNet().to(device).eval()
+    # load DQN params and set model to eval mode
+    params1_dqn = torch.load("models/dqn_{}_{}_{}.pt".format(asset_type, 2006, 2010))
+    params2_dqn = torch.load("models/dqn_{}_{}_{}.pt".format(asset_type, 2011, 2015))
+    dqn = PolicyNet().to(device).eval()
+
+    # load PG params and set model to eval mode
+    params1_pg = torch.load("models/pg_{}_{}_{}.pt".format(asset_type, 2006, 2010))
+    params2_pg = torch.load("models/pg_{}_{}_{}.pt".format(asset_type, 2011, 2015))
+    pg = PolicyNet().to(device).eval()
 
     min_year_idx = min_year % 2006 + 1
     max_year_idx = max_year % 2006 + 1
@@ -43,11 +48,13 @@ if __name__ == '__main__':
     tgt_vol = torch.tensor(0.1, device=device)
 
     dqn_returns = []
+    pg_returns = []
     lo_returns = []
     macd_returns = []
     sgn_returns = []
 
     dqn_actions_prev = torch.zeros(len(S), device=device)
+    pg_actions_prev = torch.zeros(len(S), device=device)
     sgn_actions_prev = torch.zeros(len(S), device=device)
 
     # long only actions
@@ -59,14 +66,17 @@ if __name__ == '__main__':
 
     # set initial model params
     if min_idx < switch_idx:
-        model.load_state_dict(params1)
+        dqn.load_state_dict(params1_dqn)
+        pg.load_state_dict(params1_pg)
     else:
-        model.load_state_dict(params2)
+        dqn.load_state_dict(params2_dqn)
+        pg.load_state_dict(params2_pg)
 
     for t in test_idx:
         # change the model if we reach switch_idx
         if t == switch_idx:
-            model.load_state_dict(params2)
+            dqn.load_state_dict(params2_dqn)
+            pg.load_state_dict(params2_pg)
 
         states = S[:, t]
         prices = P[:, t]
@@ -74,10 +84,15 @@ if __name__ == '__main__':
         sigmas = sigall[:, t+1]
         sigmas_prev = sigall[:, t]
 
-        dqn_actions = model.get_actions(states)
+        dqn_actions = dqn.get_actions(states)
         dqn_rewards = get_reward(prices, prices_next, sigmas, sigmas_prev, dqn_actions, dqn_actions_prev, tgt_vol, bp)
         dqn_returns.append(dqn_rewards.mean().item())
         dqn_actions_prev = dqn_actions
+
+        pg_actions = pg.get_actions(states)
+        pg_rewards = get_reward(prices, prices_next, sigmas, sigmas_prev, pg_actions, pg_actions_prev, tgt_vol, bp)
+        pg_returns.append(pg_rewards.mean().item())
+        pg_actions_prev = pg_actions
 
         lo_rewards = get_reward(prices, prices_next, sigmas, sigmas_prev, lo_actions, lo_actions, tgt_vol, bp)
         lo_returns.append(lo_rewards.mean().item())
@@ -92,10 +107,11 @@ if __name__ == '__main__':
         sgn_actions_prev = sgn_actions
 
     plt.plot(np.cumsum(dqn_returns), color='red')
+    plt.plot(np.cumsum(pg_returns), color='purple')
     plt.plot(np.cumsum(lo_returns), color='blue')
     plt.plot(np.cumsum(macd_returns), color='green')
     plt.plot(np.cumsum(sgn_returns), color='orange')
-    plt.legend(['DQN', 'Long-Only', 'MACD', 'Sign(R)'])
+    plt.legend(['DQN', 'PG', 'Long-Only', 'MACD', 'Sign(R)'])
 
     if asset_type == 'all':
         asset_title = 'all assets'
@@ -113,7 +129,7 @@ if __name__ == '__main__':
     tick_idx = np.arange(0, (max_year-min_year)*252+1, 252)
     plt.xticks(tick_idx, x_labels)
 
-    plt.savefig("plots/dqn_{}_{}_{}.png".format(asset_type, min_year, max_year),
+    plt.savefig("plots/{}_{}_{}.png".format(asset_type, min_year, max_year),
     format='png', bbox_inches='tight')
 
     plt.show()
