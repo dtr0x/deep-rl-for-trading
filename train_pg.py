1695ae131442149f19b533fc1bf49ed12acdc3db
Dylan Troop <dylantroop@gmail.com>
2020-11-13 13:16:27 +0000

    Policy gradient training update.

--- train_pg.py
+++ train_pg.py
@@ -9,6 +9,19 @@ import time, argparse
 # set device for optimization
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Select action by sampling from policy
+def sample_action(policy_net, state):
+    probs = softmax(policy_net(state.unsqueeze(0)), dim=1)
+    u = torch.rand(1, device=device) # sample uniform value
+    cdf = probs.cumsum(dim=1)
+    if u <= cdf[0,0]:
+        action_idx = 0
+    elif u <= cdf[0,1]:
+        action_idx = 1
+    else:
+        action_idx = 2
+    return action_idx-1, probs[0, action_idx]
+
 def train_pg(policy_net, optimizer, asset_type, min_year=2006, max_year=2010,
     val_frac=0.1, discount_factor=0.3, n_epochs=20, bp=0.002, tgt_vol=0.1, num_epochs=20):
     # durations to train/validate for
@@ -26,15 +39,15 @@ def train_pg(policy_net, optimizer, asset_type, min_year=2006, max_year=2010,
     S, P, sigall = [x.to(device) for x in load_states(asset_type)]
     n_assets = len(S)
 
-    actions_prev = torch.zeros(n_assets, device=device)
-
     for i_epoch in range(num_epochs):
         policy_net.train()
         t1 = time.time()
         # Store rewards and actions for this trajectory (epoch)
         rewards_all = torch.zeros((n_steps, n_assets), dtype=torch.float32, device=device)
         actions_all = torch.zeros((n_steps, n_assets), dtype=torch.long, device=device)
+        logprobs_all = torch.zeros((n_steps, n_assets), dtype=torch.float32, device=device)
 
+        actions_prev = torch.zeros(n_assets, device=device)
         step = 0 # for indexing rewards/actions
         # loop to store trajectories
         for t in train_idx:
@@ -46,8 +59,17 @@ def train_pg(policy_net, optimizer, asset_type, min_year=2006, max_year=2010,
             sigmas = sigall[:, t+1]
             sigmas_prev = sigall[:, t]
 
-            # get actions for each asset
-            actions = policy_net.get_actions(states)
+            # get actions and logprobs for each asset
+            for i in range(n_assets):
+                s = states[i]
+                action, prob = sample_action(policy_net, s)
+                actions_all[step, i] = action
+                logprobs_all[step, i] = prob.log()
+
+            actions = actions_all[step]
+
+            if step % 100 == 0:
+                print(actions)
 
             # compute rewards for each asset
             rewards = get_reward(prices, prices_next, sigmas, sigmas_prev, actions, actions_prev, tgt_vol, bp)
@@ -66,20 +88,13 @@ def train_pg(policy_net, optimizer, asset_type, min_year=2006, max_year=2010,
             next_states = S[:, t+1]
             rewards = rewards_all[step:]
             actions = actions_all[step]
+            logprobs = logprobs_all[step]
 
             gamma_exp = torch.tensor([discount_factor**i for i in range(len(rewards))], device=device)
 
             g = (gamma_exp.unsqueeze(1) * rewards).sum(axis=0)
 
-            # get values of currrent actions from policy net
-            action_values = policy_net(states)
-            probs = softmax(action_values, dim=1)
-            action_idx = actions.long() + 1 # action indexes
-            row_idx = torch.arange(probs.size(0))
-            probs = probs[row_idx, action_idx]
-            log_probs = probs.log()
-
-            policy_grad_op = discount_factor**step * g * log_probs
+            policy_grad_op = discount_factor**step * g * logprobs
 
             optimizer.zero_grad() # set gradients to 0 to avoid accumulation
 
@@ -129,14 +144,17 @@ def train_pg(policy_net, optimizer, asset_type, min_year=2006, max_year=2010,
 
 if __name__ == '__main__':
     # get arguments from command line for asset type and year range
-    parser = argparse.ArgumentParser(description='Select asset type and year range to train.')
-    parser.add_argument('-asset_type', type=str, required=True)
-    parser.add_argument('-min_year', type=int, required=True)
-    parser.add_argument('-max_year', type=int, required=True)
-    args = parser.parse_args()
-    asset_type = args.asset_type
-    min_year = args.min_year
-    max_year = args.max_year
+    # parser = argparse.ArgumentParser(description='Select asset type and year range to train.')
+    # parser.add_argument('-asset_type', type=str, required=True)
+    # parser.add_argument('-min_year', type=int, required=True)
+    # parser.add_argument('-max_year', type=int, required=True)
+    # args = parser.parse_args()
+    # asset_type = args.asset_type
+    # min_year = args.min_year
+    # max_year = args.max_year
+    asset_type = 'index'
+    min_year = 2006
+    max_year = 2010
 
     # set up networks, memory, optimizer
     policy_net = PolicyNet().to(device)
